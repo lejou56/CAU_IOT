bf634de8ee6b1b0a7fd5051b641d065c9abbfeba
lejou56 <lejou56@gmail.com>
2024-04-30 15:46:25 +0900

    기존 정보를 복사해서 새로운 객체를 반환하는 함수(얕은 복사)

---
+++ Ch.1/1-12.js
@@ -0,0 +1,10 @@
+var copyObject = function(target) {
+    var result = {};
+    for (var prop in target) {
+        result[prop] = target[prop];
+ 
+    }
+    return result
+};
+
+module.exports = copyObject;
