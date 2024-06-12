ac0e67ffc3a8a68d605fafe41b773f17e24462be
lejou56 <lejou56@gmail.com>
2024-06-13 01:43:37 +0900

    클래스 상속 및 추상화 방법(1) - 인스턴스 생성 후 프로퍼티 제거

---
+++ Ch.7/7-08.js
@@ -0,0 +1,30 @@
+var extendClass1 = function (SuperClass, SubClass, subMethods) {
+    SubClass.prototype = new SuperClass();
+    for (var prop in SubClass.prototype) {
+        if (SubClass.prototype.hasOwnProperty(prop)) {
+            delete SubClass.prototype[prop];
+        }
+    }
+    if (subMethods) {
+        for (var method in subMethods) {
+            SubClass.prototype[method] = subMethods[method];
+        }
+    }
+    Object.freeze(SubClass.prototype);
+    return SubClass; 
+};
+
+var Rectangle = function (width, height) {
+    this.width = width;
+    this.height = height;
+};
+Rectangle.prototype.getArea = function () {
+    return this.width * this.height;
+};
+var Square = extendClass1(Rectangle, function (width) {
+    Rectangle.call(this, width, width);
+});
+var Bridge = function () {};
+Bridge.prototype = Rectangle.prototype;
+Square.prototype = new Bridge();
+Object.freeze(Square.prototype);
